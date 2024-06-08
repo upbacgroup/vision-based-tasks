03c35782559a96395d9e9adcbf906f7fd9c010c7
Automatic Control Group - EIM - University of Paderborn <152388735+upbacgroup@users.noreply.github.com>
2024-06-08 09:57:58 +0200

    Create test.py

---
+++ src/test.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+if __name__ == '__main__':
+    print('Check length of a single line of code (LOC), '
+          'and we expect this LOC is too long '
+          'because it contains more than 80 characters:',
+          np.array([1, 2, 3, 4, 5, 6, 7, 8]))
